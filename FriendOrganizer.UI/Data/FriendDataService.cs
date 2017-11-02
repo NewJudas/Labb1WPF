695cb7cb1f5e679558af0257b31cad1e4604de4c
Unknown <kenan.alic@telia.com>
2017-11-02 17:01:52 +0100

    Stage 4

---
+++ FriendOrganizer.UI/Data/FriendDataService.cs
@@ -0,0 +1,16 @@
+﻿using FriendOrganizer.Model;
+using System.Collections.Generic;
+
+namespace FriendOrganizer.UI.Data
+{
+    public class FriendDataService : IFriendDataService
+    {
+        public IEnumerable<Friend> GetAll()
+        {
+            yield return new Friend { FirstName = "Kenan", LastName = "Alic" };
+            yield return new Friend { FirstName = "Bosse", LastName = "Kaffekopp" };
+            yield return new Friend { FirstName = "Nils", LastName = "Pyssling" };
+            yield return new Friend { FirstName = "Thomas", LastName = "Huber" };
+        }
+    }
+}
